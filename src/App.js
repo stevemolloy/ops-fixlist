3f22ae0415243cdd097fd879f963b9fd37dec232
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-05-10 16:52:23 +0200

    First addition of a basic form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="primary">Primary</Button>
+        <IssueForm/>
       </header>
     </div>
   );
+}
+
+const IssueForm = props => {
+  const [submitter, setSubmitter] = useState("")
+  const [issue, setIssue] = useState("")
+  const [otherinfo, setOtherinfo] = useState("")
+
+  const handleSubmit = event => {
+    alert(submitter + issue + otherinfo)
+    event.preventDefault()
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Submitter</Form.Label>
+        <Form.Control type="text" placeholder="Your name" onChange={e => setSubmitter(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Issue</Form.Label>
+        <Form.Control type="textarea" placeholder="Issue" onChange={e => setIssue(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Other information</Form.Label>
+        <Form.Control type="textarea" placeholder="Other information" onChange={e => setOtherinfo(e.target.value)}/>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  )
 }
 
 export default App;
