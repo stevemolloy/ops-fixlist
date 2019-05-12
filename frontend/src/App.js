d021a6c1e90075827b1086bbb685a68e3275aebc
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-05-12 23:41:25 +0200

    Shows the table from the database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Table from 'react-bootstrap/Table';
+import axios from "axios";
 
 import IssueForm from './IssueForm';
 import './App.css';
 
 function App() {
   const [show, setShow] = useState(false);
+  const [issueList, setIssueList] = useState([]);
 
   useEffect(() => {
+    axios
+      .get("http://localhost:9000/issues")
+      .then(resp => {
+        console.log(resp.data);
+        setIssueList(resp.data);
+      })
     console.log('useEffect');
-  })
+  }, [])
 
   return (
     <div className="App">
+      <Table striped border hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Submitter</th>
+            <th>Description</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {issueList.map(issue => {
+            return (
+              <tr>
+                <td>#</td>
+                <td>{issue.submitter}</td>
+                <td>{issue.description}</td>
+                <td>{issue.other_info}</td>
+              </tr>
+            )})
+          }
+        </tbody>
+      </Table>
       <Button variant="primary" onClick={() => setShow(true)}>
         New issue
       </Button>
