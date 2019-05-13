1ca9e48feecf29c28ddbaf7dc593ac833d697a4f
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-05-13 14:02:05 +0200

    Reduced the drilling a little

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 import axios from "axios";
 
-import Header from "./Header";
-import Body from "./Body";
+import InfoTable from "./InfoTable";
+import NewIssueModal from "./NewIssueModal";
 import './App.css';
 
 function App() {
@@ -24,12 +25,20 @@ function App() {
     <div className="App">
       <Container>
         <Row>
-          <Header/>
+          <div className="AppHeader">
+            <h1>Operations Fix-List</h1>
+          </div>
         </Row>
         <Row>
-          <Body setIssueList={setIssueList} issueList={issueList} show={show} setShow={setShow} />
+          <div className="AppBody">
+            <Button variant="primary" onClick={() => setShow(true)}>
+              New issue
+            </Button>
+            <InfoTable issueList={issueList}/>
+          </div>
         </Row>
       </Container>
+      <NewIssueModal setIssueList={setIssueList} show={show} setShow={setShow}/>
     </div>
   );
 }
