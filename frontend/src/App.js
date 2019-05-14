4060867d34af2e40d00f28a12c4c30f1b91536da
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-05-14 16:52:48 +0200

    Router working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,22 @@ import InfoTable from "./InfoTable";
 import NewIssueModal from "./NewIssueModal";
 import './App.css';
 
-const NotFound = () => <div>Not found</div>
-
 const App = () => {
   const [show, setShow] = useState(false);
   const [issueList, setIssueList] = useState([]);
+
+  const Content = () => {
+    return (
+      <div className="AppBody">
+        <Button variant="primary" onClick={() => setShow(true)}>
+          New issue
+        </Button>
+        <InfoTable issueList={issueList}/>
+      </div>
+    );
+  };
+
+  const NotFound = () => <div>Issue not found</div>;
 
   useEffect(() => {
     axios
@@ -26,22 +37,22 @@ const App = () => {
 
   return (
     <div className="App">
-      <Container>
-        <Row>
-          <div className="AppHeader">
-            <h1>Operations Fix-List</h1>
-          </div>
-        </Row>
-        <Row>
-          <div className="AppBody">
-            <Button variant="primary" onClick={() => setShow(true)}>
-              New issue
-            </Button>
-            <InfoTable issueList={issueList}/>
-          </div>
-        </Row>
-      </Container>
-      <NewIssueModal setIssueList={setIssueList} show={show} setShow={setShow}/>
+      <BrowserRouter>
+        <Container>
+          <Row>
+            <div className="AppHeader">
+              <h1>Operations Fix-List</h1>
+            </div>
+          </Row>
+          <Row>
+            <Switch>
+              <Route exact path="/" component={Content} />
+              <Route component={NotFound} />
+            </Switch>
+          </Row>
+        </Container>
+        <NewIssueModal setIssueList={setIssueList} show={show} setShow={setShow}/>
+      </BrowserRouter>
     </div>
   );
 }
