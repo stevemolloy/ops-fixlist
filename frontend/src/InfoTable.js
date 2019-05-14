143b93747f3b9ce26b374715c9de6bbd6fce55b1
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-05-14 14:29:16 +0200

    Traffic lights work

--- frontend/src/InfoTable.js
+++ frontend/src/InfoTable.js
@@ -18,7 +18,9 @@ const InfoTable = props => {
           return (
             <tr key={issue.id}>
               <td>{index+1}</td>
-              <td className="led-box"><div className="led-yellow"></div></td>
+              <td className="led-box">
+                <div className={"led-" + trafficLightColor(issue.createdAt)}></div>
+              </td>
               <td>{issue.submitter}</td>
               <td>{issue.description}</td>
               <td>{issue.other_info}</td>
@@ -28,6 +30,31 @@ const InfoTable = props => {
       </tbody>
     </Table>
   )
+}
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// a and b are javascript Date objects
+const dateDiffInDays = (a, b) => {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+const trafficLightColor = (a) => {
+  const a_Date = new Date(a);
+  const b_Date = new Date(); // Now
+  const diff = dateDiffInDays(a_Date, b_Date);
+
+  if (diff < 1) {
+    return 'green';
+  } else if (diff < 2) {
+    return 'yellow';
+  } else {
+    return 'red';
+  }
 }
 
 export default InfoTable;
