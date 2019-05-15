457a41f2f1ddcabd423f6d3ce621b981434039a7
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-05-15 11:14:16 +0200

    IssuePage shows the full issue in a table

--- frontend/src/IssuePage.js
+++ frontend/src/IssuePage.js
@@ -12,11 +12,31 @@ const IssuePage = ({match}) => {
       .then(resp => {
         setIssue(resp.data);
       });
-  }, []);
+  }, [issueid]);
+
+  const createdDateTime = new Date(issue.createdAt);
 
   return (
-
-    <div><h1>{issue.description}</h1></div>
+    <Table variant="dark" borderless striped hover>
+      <tbody>
+        <tr>
+          <td><strong>Submitter</strong></td>
+          <td>{issue.submitter}</td>
+        </tr>
+        <tr>
+          <td><strong>Date submitted</strong></td>
+          <td>{createdDateTime.toString()}</td>
+        </tr>
+        <tr>
+          <td><strong>Description</strong></td>
+          <td>{issue.description}</td>
+        </tr>
+        <tr>
+          <td><strong>Other information</strong></td>
+          <td>{issue.other_info}</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
