a51425ee9beeb4846805fa50fe8d7db205246354
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-05-16 14:41:42 +0200

    Everything except submit works on the issueedit form

--- frontend/src/EditIssue.js
+++ frontend/src/EditIssue.js
@@ -3,6 +3,8 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
+import NotFound from './NotFound';
+
 const EditIssue = ({match}) => {
   const [issue, setIssue] = useState({});
   const [notfound, setNotfound] = useState(false);
@@ -20,21 +22,52 @@ const EditIssue = ({match}) => {
       });
   }, [issueid]);
 
+  if (notfound) {
+    return <NotFound/>;
+  }
+
+  const submitter_handler = (event) => {
+    setIssue({...issue, submitter: event.target.value});
+  }
+
+  const description_handler = (event) => {
+    setIssue({...issue, description: event.target.value});
+  }
+
+  const other_info_handler = (event) => {
+    setIssue({...issue, other_info: event.target.value});
+  }
+
   return (
     <Form>
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Submitter</Form.Label>
-        <Form.Control type="text" defaultValue={issue.submitter} />
+        <Form.Control
+          as="textarea"
+          rows="1"
+          cols="40"
+          value={issue.submitter}
+          onChange={submitter_handler}
+        />
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Issue</Form.Label>
-        <Form.Control type="textarea" defaultValue={issue.description} />
+        <Form.Control
+          as="textarea"
+          rows="7"
+          value={issue.description}
+          onChange={description_handler}
+        />
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Other information</Form.Label>
-        <Form.Control type="textarea" defaultValue={issue.other_info} />
+        <Form.Control
+          as="textarea"
+          value={issue.other_info}
+          onChange={other_info_handler}
+        />
       </Form.Group>
 
       <Button variant="primary" type="submit">
