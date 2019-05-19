34bde409ee38710474a8ab7ce3bdb0dcf94bda09
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-05-19 15:07:09 +0200

    Refactored stuff out of App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import axios from "axios";
 
-import InfoTable from "./InfoTable";
-import NewIssueModal from "./NewIssueModal";
 import IssuePage from './IssuePage';
 import EditIssue from './EditIssue';
+import FrontPage from './FrontPage';
 import './App.css';
-
-const FrontPage = () => {
-  const [show, setShow] = useState(false);
-  const [issueList, setIssueList] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:9000/issues")
-      .then(resp => {
-        console.log(resp);
-        setIssueList(resp.data);
-      })
-  }, []);
-
-  return (
-    <div className="AppBody">
-      <Button variant="primary" onClick={() => setShow(true)}>
-        New issue
-      </Button>
-      <h2>Outstanding Issues</h2>
-      <InfoTable issueList={issueList.filter(issue => !issue.resolved)}/>
-      <h2>Resolved Issues</h2>
-      <InfoTable issueList={issueList.filter(issue => issue.resolved)}/>
-      <NewIssueModal setIssueList={setIssueList} show={show} setShow={setShow}/>
-    </div>
-  );
-};
 
 const App = () => {
   return (
---
+++ frontend/src/FrontPage.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import axios from "axios";
+
+import InfoTable from "./InfoTable";
+import NewIssueModal from "./NewIssueModal";
+
+const FrontPage = () => {
+  const [show, setShow] = useState(false);
+  const [issueList, setIssueList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/issues")
+      .then(resp => {
+        setIssueList(resp.data);
+      })
+  }, []);
+
+  return (
+    <div className="AppBody">
+      <Button variant="primary" onClick={() => setShow(true)}>
+        New issue
+      </Button>
+      <h2>Outstanding Issues</h2>
+      <InfoTable issueList={issueList.filter(issue => !issue.resolved)}/>
+      <h2>Resolved Issues</h2>
+      <InfoTable issueList={issueList.filter(issue => issue.resolved)}/>
+      <NewIssueModal setIssueList={setIssueList} show={show} setShow={setShow}/>
+    </div>
+  );
+};
+
+export default FrontPage;
